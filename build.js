e907c75d5d41b811a9ec9a4d919d89b4962976b1
Gubbi Labs <sudhira@gubbilabs.in>
2017-01-26 09:15:27 +0530

    Update build.js

--- build.js
+++ build.js
@@ -44,47 +44,9 @@ var map = new mapboxgl.Map({
 // var geolocate = map.addControl(new mapboxgl.Geolocate({
 //    position: 'bottom-right'
 // }));
-// map.addControl(new mapboxgl.Navigation());
+map.addControl(new mapboxgl.Navigation());
 
-var myLayer = L.mapbox.featureLayer().addTo(map);
-	
-	if (!navigator.geolocation) {
-    geolocate.innerHTML = 'Geolocation is not available';
-} else {
-    geolocate.onclick = function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        map.locate();
-    };
-}
-
-// Once we've got a position, zoom and center the map
-// on it, and add a single marker.
-map.on('locationfound', function(e) {
-    map.fitBounds(e.bounds);
-
-    myLayer.setGeoJSON({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [e.latlng.lng, e.latlng.lat]
-        },
-        properties: {
-            'title': 'Here I am!',
-            'marker-color': '#ff8888',
-            'marker-symbol': 'star'
-        }
-    });
-
-    // And hide the geolocation button
-    geolocate.parentNode.removeChild(geolocate);
-});
-
-// If the user chooses not to allow their location
-// to be shared, display an error message.
-map.on('locationerror', function() {
-    geolocate.innerHTML = 'Position could not be found';
-});
+map.addControl(new mapboxgl.GeolocateControl({ position: 'bottom-right' }));
 
 // Layer for review markers
 var overlayDataSource = new mapboxgl.GeoJSONSource({
