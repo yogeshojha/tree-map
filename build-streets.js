21914d92372ebf9f1722253666e39b73cb0c71db
Gubbi Labs <sudhira@gubbilabs.in>
2017-02-05 10:21:23 +0530

    Update build-streets.js
    Moved the save feature buttons up.

--- build-streets.js
+++ build-streets.js
@@ -114,7 +114,7 @@ map.on('style.load', function(e) {
 				
 				var formTreename = "<fieldset><label>Tree Name: <span id='treename' style='background-color:#eee'></span></label><input type='text' name='treename' placeholder='treename'></input></fieldset>"
 				
-				var formHeight = "<fieldset><label>Height (metres): <span id='height' style='background-color:#eee'></span></label><input type='text' name='height' placeholder='height'></input></fieldset>"
+				var formHeight = "<fieldset><label><strong>Additional attributes:</strong><br/>Height (metres): <span id='height' style='background-color:#eee'></span></label><input type='text' name='height' placeholder='height'></input></fieldset>"
 				
 				var formGirth = "<fieldset><label>Girth (cm): <span id='girth' style='background-color:#eee'></span></label><input type='text' name='girth' placeholder='girth'></input></fieldset>"
 				
@@ -124,7 +124,8 @@ map.on('style.load', function(e) {
 			
                 var formReviewer = "<fieldset><label>Contributed by: <span id='reviewer' style='padding:5px;background-color:#eee'></span></label><input type='text' name='reviewer' placeholder='name'></input></fieldset>"
 				
-                var popupHTML = "<form>" + formOptions + formReviewer + formTreename + formHeight + formGirth + formCanopywidth + formConfidence + "<a id='updateOverlayFeature' class='button col4' href='#'>Save</a><a id='deleteOverlayFeature' class='button quiet fr col4' href='#' style=''>Delete</a></form>";
+                // var popupHTML = "<form>" + formOptions + formReviewer + formTreename + formHeight + formGirth + formCanopywidth + formConfidence + "<a id='updateOverlayFeature' class='button col4' href='#'>Save</a><a id='deleteOverlayFeature' class='button quiet fr col4' href='#' style=''>Delete</a></form>";
+		var popupHTML = "<form>" + formOptions + formReviewer + formTreename + "<a id='updateOverlayFeature' class='button col4' href='#'>Save</a><a id='deleteOverlayFeature' class='button quiet fr col4 icon check fill-red' href='#' style=''>Delete</a></form>" + formHeight + formGirth + formCanopywidth + formConfidence + "</form>";    
                 var popup = new mapboxgl.Popup()
                     .setLngLat(e.lngLat)
                     .setHTML(popupHTML)
